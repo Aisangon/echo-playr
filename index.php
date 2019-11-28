4728f0330468ad7a1e9c54c54609cda0f4d5d298
Ainel <ainelainel@hotmail.com>
2019-11-28 22:01:40 +0100

    create album page

--- index.php
+++ index.php
@@ -8,10 +8,12 @@
             while($row = mysqli_fetch_array($albumQuery)) {
                 echo "<div class='col-2 mb-3'>
                         <div class='card bg-dark border-secondary'>
-                        <img class='card-img-top' src='" . $row['artworkPath'] ."'> 
-                            <div class='card-body p-1 bg-dark'>
-                                <p class='card-text text-light text-truncate'>" . $row['title'] ." </p>
-                            </div>
+                            <a class='text-decoration-none' href='album.php?id=" . $row['id'] . "'>
+                                <img class='card-img-top' src='" . $row['artworkPath'] ."'>
+                                <div class='card-body p-1 bg-dark'>
+                                    <p class='card-text text-light text-truncate'>" . $row['title'] ." </p>
+                                </div>
+                            </a>
                         </div>
                     </div>";
             }
